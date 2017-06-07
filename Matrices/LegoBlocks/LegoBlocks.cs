cbc3e6425faebd73ffdf18d9e3ae1159d24c3496
KristiyanVasilev <make_05@abv.bg>
2017-06-07 18:40:09 +0300

    Add new matrix excercises

---
+++ Matrices/LegoBlocks/LegoBlocks.cs
@@ -0,0 +1,67 @@
+﻿namespace LegoBlocks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class LegoBlocks
+    {
+        public static void Main()
+        {
+            var separators = " ,\n\t".ToCharArray();
+            var n = int.Parse(Console.ReadLine().Trim());
+
+            var matrices = new List<int[][]>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                matrices.Add(new int[n][]);
+
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == 0)
+                        matrices[i][j] = Console.ReadLine()
+                            .Trim()
+                            .Split(separators, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(int.Parse)
+                            .ToArray();
+                    else
+                        matrices[i][j] = Console.ReadLine()
+                            .Trim()
+                            .Split(separators, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(int.Parse)
+                            .Reverse()
+                            .ToArray();
+
+                }
+            }
+
+            var result = new int[n][];
+            for (int i = 0; i < 1; i++)
+                for (int j = 0; j < n; j++)
+                    result[j] = matrices[i][j]
+                        .Concat(matrices[i + 1][j]).ToArray();
+
+            var cols = result[0].Length;
+            bool rectangle = result.All(row => cols == row.Length);
+
+            if (rectangle)
+            {
+                foreach (var row in result)
+                {
+                    Console.WriteLine('[' + string.Join(", ", row) + ']');
+                }
+            }
+            else
+            {
+                var total = 0;
+                foreach (var row in result)
+                    total += row.Length;
+
+                Console.WriteLine($"The total number of cells is: {total}");
+            }
+        }
+    }
+}
---
+++ Matrices/TargetPractice/TargetPractice.cs
@@ -0,0 +1,126 @@
+﻿namespace TargetPractice
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class TargetPractice
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split();
+
+            var rows = int.Parse(input[0]);
+            var cols = int.Parse(input[1]);
+
+            var matrix = new char[rows, cols];
+            var snake = Console.ReadLine();
+            FillMatrix(rows, cols, matrix, snake);
+
+            var shotTokens = Console.ReadLine().Split(' ');
+            var impactRow = int.Parse(shotTokens[0]);
+            var impactCol = int.Parse(shotTokens[1]);
+            var radius = int.Parse(shotTokens[2]);
+
+            FireShot(matrix, impactRow, impactCol, radius);
+             
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+
+                RunGravity(matrix, col);
+            }
+
+            PrintMatrix(matrix);
+        }
+
+        private static void RunGravity(char[,] matrix, int col)
+        {
+            while (true)
+            {
+                var hasFallen = false;
+                for (int row = 1; row < matrix.GetLength(0); row++)
+                {
+                    var topChar = matrix[row - 1, col];
+                    var currentChar = matrix[row, col];
+
+                    if (currentChar == ' ' && topChar != ' ')
+                    {
+                        matrix[row, col] = topChar;
+                        matrix[row - 1, col] = ' ';
+                        hasFallen = true;
+                    }
+                }
+                if (!hasFallen)
+                {
+                    break;
+                }
+            }
+        }
+
+        private static void FireShot(char[,] matrix, int impactRow, int impactCol, int radius)
+        {            
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if ((col - impactCol) * (col - impactCol) + (row - impactRow) * (row - impactRow) <= (radius * radius))
+                    {
+                        matrix[row, col] = ' ';
+                    }
+                }
+            }
+        }
+
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void FillMatrix(int rows, int cols, char[,] matrix, string snake)
+        {
+            var isMovingLeft = true;
+            var currentIndex = 0;
+
+            for (int row = rows - 1; row >= 0; row--)
+            {
+                if (isMovingLeft)
+                {
+                    for (int col = cols - 1; col >= 0; col--)
+                    {
+                        if (currentIndex >= snake.Length)
+                        {
+                            currentIndex = 0;
+                        }
+
+                        matrix[row, col] = snake[currentIndex];
+                        currentIndex++;
+                    }
+                }
+                else
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        if (currentIndex >= snake.Length)
+                        {
+                            currentIndex = 0;
+                        }
+
+                        matrix[row, col] = snake[currentIndex];
+                        currentIndex++;
+                    }
+                }
+                isMovingLeft = !isMovingLeft;
+            }
+        }
+    }
+}
